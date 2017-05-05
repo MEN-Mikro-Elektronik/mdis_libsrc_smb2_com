efdee0f3c6ec7ea40cb53d31dc98753e7fd9a9b1
thomas.schnuerer <thomas.schnuerer@men>
2017-05-05 14:47:03 +0200

    added GPL license notification under every MEN document header

--- smb2_menz001.c
+++ smb2_menz001.c
@@ -109,7 +109,21 @@
  * based on sysmanagbus_ali1543.c, Revision 1.9  (2000/08/08)
  *---------------------------------------------------------------------------
  * (c) Copyright 2005 by MEN Mikro Elektronik GmbH, Nuremberg, Germany
- ****************************************************************************/
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* #define DBG */
 #include <MEN/men_typs.h>
